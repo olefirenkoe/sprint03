0ca55b5f36cec92e254ff886ee53a75fe510deb2
Yevhen Olefirenko <yolefirenk@e1r7p10.unit.ua>
2020-11-16 20:22:07 +0200

    modified t10

--- t10/js/script.js
+++ t10/js/script.js
@@ -6,7 +6,8 @@
   let banner;
   let i = 0;
 
-  function makeBannerAgain() {
+  function makeBannerAgain(a) {
+      i = a;
       banner = setInterval(function() {
           divPhoto.innerHTML = `<img class="img" src="assets/images/${heroMassiv[i]}.jpg" alt="superhero">`;
           if (i == 2) {
@@ -17,7 +18,7 @@
 
   }
 
-  makeBannerAgain();
+  makeBannerAgain(0);
 
   function nextOrPrevious() {
       let target = event.target;
@@ -25,25 +26,25 @@
       if (target.getAttribute('data-elemnt') == 'next') {
           if (divPhoto.innerHTML == `<img class="img" src="assets/images/superman.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/batman.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(2);
           } else if (divPhoto.innerHTML == `<img class="img" src="assets/images/batman.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/spider.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(0);
           } else if (divPhoto.innerHTML == `<img class="img" src="assets/images/spider.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/superman.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(1);
           }
       }
       if (target.getAttribute('data-elemnt') == 'previous') {
           if (divPhoto.innerHTML == `<img class="img" src="assets/images/spider.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/batman.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(1);
           } else if (divPhoto.innerHTML == `<img class="img" src="assets/images/batman.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/superman.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(0);
           } else if (divPhoto.innerHTML == `<img class="img" src="assets/images/superman.jpg" alt="superhero">`) {
               divPhoto.innerHTML = `<img class="img" src="assets/images/spider.jpg" alt="superhero">`;
-              makeBannerAgain();
+              makeBannerAgain(2);
           }
       }
   }
